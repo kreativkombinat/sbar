6012c836ae0d3e08963e5607877ee88527e6fbf2
Serge Davidov <sd@kreativkombinat.de>
2013-01-17 14:27:40 +0100

    update

--- send.php
+++ send.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>KREATIVKOMBINAT - ON/OFF-LINE MEDIEN AGENTUR - V.9</title>
+  <title>S-BAR sushi &amp; cocktails</title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
   <meta name="viewport" content="width=device-width" />
-  <meta name="description" content="Kreativkombinat ist eine Fullservice-Agentur aus dem Bereich interaktive &amp; klassiche Medien mit Sitz in Augsburg. Auf Basis von Open-Source-Komponenten wie Python, Django, Zope, Plone, TYPO3 und PHP setzen wir Internetportale, Webseiten, Webapplikationen, Intranet und Shopl&ouml;sungen um. Im Bereich der klassischen Werbung entwickeln wir von Werbekonzepten &uuml;ber Corporate Identity (CI) bis hin zu Werbemitteln wie Flyer oder Kataloge - alles zur Zufriedenheit unserer Kunden. Kreativ, schnell und kosteng&uuml;nstig." />
-  <meta name="keywords" content="web, design, kreativ, strategy, development, markenentwicklung, werbung, marketing, communication, html, css, online, internet, cms, plone" />
+  <meta name="keywords" content="Sushi,Sushibar,Sbar,S-bar,esbar,essbar, Japanisch,suschi,essen, restaurant,bar,augsburg,speise,speisen,lokal,sushilokal,sushirestaurant,thai,food,asia,cocktails,happyhour,drinks,drink,trinken,spaß,spass,lounge,dinner,nightlife,">
+  <meta name="description" content="S-bar ist das Sushi-Restaurant und die Sushi Bar in Augsburg. Zahlreiche Sushi-Gerichte im Angebot - natürlich auch als Take Away. Wir führen neben Sushi auch viele Thai-Spezialitäten und Asia-Food. Location: Augsburg Ecke Klinkertor und Am Katzenstadl.">
   <meta name="robots" content="All" /> 
   <meta name="author" content="Serge Davidov, sd@kreativkombinat.de" />
 
