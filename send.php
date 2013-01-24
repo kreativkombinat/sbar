470be5c6f841dac34f9a1d998d167a754573f47c
Serge Davidov <sd@kreativkombinat.de>
2013-01-24 14:25:16 +0100

    update

--- send.php
+++ send.php
@@ -37,8 +37,7 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 $text = "NAME: $name<br>
-         EMAIL: $email<br>
-		 URL: $url<br>		 
+         EMAIL: $email<br>	 
          MESSAGE: $message";
 $headers = "MIME-Version: 1.0" . "\r\n"; 
 $headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
