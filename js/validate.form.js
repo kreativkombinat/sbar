0ff29767bd34d5c66043020a0b179ef793889fc5
Serge Davidov <sd@kreativkombinat.de>
2013-01-24 14:11:09 +0100

    form update

--- js/validate.form.js
+++ js/validate.form.js
@@ -55,7 +55,9 @@
 	                success:    function(html) {
 	                    jQuery("#contact_form").slideUp("slow")
 	                    jQuery("#submit_button").attr("disabled", "disabled");
-	                    jQuery("#contact_form").after("<h3 class='center' id='send_message'>Message Sent!</h3>");
+	                    jQuery("#contact_form").after("<h3 class='center' id='send_message'>Vielen Dank f&uuml;r Ihre Nachricht.<br />
+Wir werden uns umgehend um Ihr Anliegen k&uuml;mmern oder wenn gew&uuml;nscht, bei Ihnen melden.<br />
+Euer SushiBar-Team</h3>");
 	                    jQuery("#send_message").fadeIn("slow");
 	                }
 	            });
